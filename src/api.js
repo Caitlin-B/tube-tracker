6a66462d9d7447cb40a59f573a073cddd587445f
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-05-23 14:00:53 +0100

    amended api req and readme

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const fetchStatus = () => {
   return axios
-    .get("https://api.tfl.gov.uk/Line/Mode/tube/Status")
+    .get(
+      "https://api.tfl.gov.uk/Line/Mode/tube/Status?app_id=cdf2be4d&app_key=2c620fbd67b0fe7202e99b15a97419d6"
+    )
     .then(({ data }) => data);
 };
