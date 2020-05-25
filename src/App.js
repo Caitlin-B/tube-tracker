882aafc1779216f2d5b661a939553835fa7b6b5f
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-05-25 14:25:32 +0100

    amended to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Router } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as api from "./api";
@@ -7,40 +7,44 @@ import AllLines from "./components/AllLines";
 import SingleLine from "./components/SingleLine";
 import { formatDate } from "./utils";
 
-class App extends Component {
-  state = { lines: {}, isLoading: true };
+const useLines = () => {
+  const [lines, setLines] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-  render() {
-    const { lines, isLoading } = this.state;
-
-    return (
-      <div className="App">
-        <h1 className="appHeader">Travel Widget</h1>
-        <div className="appBody">
-          {isLoading ? (
-            <h2>Loading...</h2>
-          ) : (
-            <div>
-              <div className="lastUpdated">
-                <p>Last Updated </p>
-                <p className="date">{formatDate(lines[0].modified)}</p>
-              </div>
-              <Router>
-                <AllLines path="/travel" lines={lines} />
-                <SingleLine path="/travel/:line" lines={lines} />
-              </Router>
-            </div>
-          )}
-        </div>
-      </div>
-    );
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     api.fetchStatus().then(lines => {
-      this.setState({ lines, isLoading: false });
-    });
-  }
+      setLines(lines);
+      setIsLoading(false)
+    })
+  }, [])
+
+  return {lines, setLines, isLoading, setIsLoading};
 }
+
+const App = () => {
+  const {lines, isLoading} = useLines()
+
+  return (
+    <div className="App">
+      <h1 className="appHeader">Tube Tracker</h1>
+      <div className="appBody">
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : (
+          <div>
+            <div className="lastUpdated">
+              <p>Last Updated </p>
+              <p className="date">{formatDate(lines[0].modified)}</p>
+            </div>
+            <Router>
+              <AllLines path="/travel" lines={lines} />
+              <SingleLine path="/travel/:line" lines={lines} />
+            </Router>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
