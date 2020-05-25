85ba43e58e716f6b1437d47e68f7944ccd8de073
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-05-25 14:28:00 +0100

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as api from "./api";
